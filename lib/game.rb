ad59edc402626bea4aa6f1cc547456055fcc6ffa
urilixo <falecomelvio@hotmail.com>
2022-01-10 22:38:27 -0300

    Change player to be an object of class Player

--- lib/game.rb
+++ lib/game.rb
@@ -3,16 +3,8 @@ class Game
   require_relative 'player'
 
   def initialize(player1 = nil, player2 = nil)
-    @player1 = player_name(player1)
-    @player2 = player_name(player2)
+    @player1 = Player.new('white', player1)
+    @player2 = Player.new('black', player2)
     @board = Board.new
-  end
-
-  def player_name(name)
-    if name.nil?
-      puts 'Please type player name: '
-      return gets.chomp if name.nil?
-    end
-    name
   end
 end
