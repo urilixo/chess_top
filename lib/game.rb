c9d73abe286f112f249be3f97b989cdf2ebee6ef
urilixo <falecomelvio@hotmail.com>
2022-01-11 04:36:51 -0300

    Add current turn and attr_reader

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,12 @@
 class Game
   require_relative 'board'
   require_relative 'player'
+  attr_reader :player1, :player2, :board, :current_turn
 
   def initialize(player1 = nil, player2 = nil)
     @player1 = Player.new('white', player1)
     @player2 = Player.new('black', player2)
     @board = Board.new
+    @current_turn = 'white'
   end
 end
