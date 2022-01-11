35e17260ca395641d2250240c4d53fd300da7f9e
urilixo <falecomelvio@hotmail.com>
2022-01-10 22:39:03 -0300

    Add player_name method and change init to reflect change

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,21 @@
 class Player
-  def initialize(color, player_name = nil)
-    @player_name = player_name unless player_name.nil?
+  require_relative 'board'
+  def initialize(color, name = nil)
+    @player_name = player_name(name)
     new_name if player_name.nil?
     @color = color
   end
+
+  def player_name(name)
+    if name.nil?
+      puts 'Please type player name: '
+      return gets.chomp if name.nil?
+    end
+    name
+  end
+
+  def select_piece(position)
+  end
+
+
 end
