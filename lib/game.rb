ec43740465957790a7fdb9d4670bf701c23ae1d9
urilixo <falecomelvio@hotmail.com>
2022-01-10 22:13:22 -0300

    Add player_name method

--- lib/game.rb
+++ lib/game.rb
@@ -3,8 +3,14 @@ class Game
   require_relative 'player'
 
   def initialize(player1 = nil, player2 = nil)
-    @player1 = set_player_name(player1)
-    @player2 = set_player_name(player2)
+    @player1 = player_name(player1)
+    @player2 = player_name(player2)
     @board = Board.new
+  end
+
+  def player_name(name)
+    return gets.chomp if name.nil?
+
+    name
   end
 end
