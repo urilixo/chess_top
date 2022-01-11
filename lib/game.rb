f2f6624d6c9155835ba654e6375022731084af3f
urilixo <falecomelvio@hotmail.com>
2022-01-10 22:11:45 -0300

    Add Game init

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,9 @@ class Game
   require_relative 'board'
   require_relative 'player'
 
-  def initialize
-    
+  def initialize(player1 = nil, player2 = nil)
+    @player1 = set_player_name(player1)
+    @player2 = set_player_name(player2)
+    @board = Board.new
   end
 end
