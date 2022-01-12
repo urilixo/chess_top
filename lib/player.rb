bc9831aac4fda63103ea9b6f8ffd9b2aa5c848a0
urilixo <falecomelvio@hotmail.com>
2022-01-12 09:12:13 -0300

    Add attr_reader to name and color

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,16 @@
 class Player
   require_relative 'board'
+  attr_reader :name, :color
+
   def initialize(color, name = nil)
-    @player_name = player_name(name)
-    new_name if player_name.nil?
+    @name = player_name(name)
     @color = color
   end
 
   def player_name(name)
     if name.nil?
       puts 'Please type player name: '
-      return gets.chomp if name.nil?
+      return gets.chomp
     end
     name
   end
