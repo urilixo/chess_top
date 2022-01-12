90d00e2604ecee60457afa2d2b1669b5000c328a
urilixo <falecomelvio@hotmail.com>
2022-01-12 05:24:41 -0300

    Add attr_accessor for valid_movements array

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Piece
-  attr_accessor :position, :color
+  attr_accessor :position, :color, :valid_movements
 
   def initialize(color, position)
     @color = color
