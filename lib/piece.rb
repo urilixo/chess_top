07ee290da1b503c2dcbfff22d3e615984d508005
urilixo <falecomelvio@hotmail.com>
2022-01-14 03:44:25 -0300

    Add return_valid method

--- lib/piece.rb
+++ lib/piece.rb
@@ -17,4 +17,21 @@ class Piece
 
     board[x][y].color == @color
   end
+
+  def return_valid(board)
+    row, col = @position
+    movement(row, col, board)
+    @valid_movements
+  end
+
+  def find_edge(row, col, direction, board, moves = [])
+    x, y = direction
+    row += x
+    col += y
+    return if row.negative? || col.negative? || row > 7 || col > 7
+    return if same_color?([row, col], board)
+
+    @valid_movements << [row, col]
+    find_edge(row, col, direction, board, moves) unless board[row][col].is_a?(Piece)
+  end
 end
