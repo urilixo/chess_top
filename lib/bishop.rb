81446b4a56a9ab0d434b8655eea79c0dce0d11de
urilixo <falecomelvio@hotmail.com>
2022-01-14 03:30:54 -0300

    Move return_valid to parent class

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -4,12 +4,6 @@ class Bishop < Piece
     @symbol = @color == 'white' ? ' ♗ ' : ' ♝ '
   end
 
-  def return_valid(board)
-    row, col = @position
-    movement(row, col, board)
-    @valid_movements
-  end
-
   def movement(row, col, board)
     directions = [[-1, -1], [-1, 1], [1, 1], [1, -1]]
     directions.each { |cell| find_edge(row, col, cell, board) }
--- lib/rook.rb
+++ lib/rook.rb
@@ -4,12 +4,6 @@ class Rook < Piece
     @symbol = @color == 'white' ? ' ♖ ' : ' ♜ '
   end
 
-  def return_valid(board)
-    row, col = @position
-    movement(row, col, board)
-    @valid_movements
-  end
-
   def movement(row, col, board)
     directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     directions.each { |cell| find_edge(row, col, cell, board)}
