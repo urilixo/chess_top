650613e0bc46925b6d16cc0b419497c189ec0a00
urilixo <falecomelvio@hotmail.com>
2022-01-14 08:11:56 -0300

    Add select_piece method

--- lib/player.rb
+++ lib/player.rb
@@ -15,7 +15,16 @@ class Player
     name
   end
 
-  def select_piece(position)
+  def select_piece(position, board)
+    begin
+      position = position.split(',').map(&:to_i)
+    rescue NoMethodError
+      puts 'Input a value in the format number,number. E.g. 2,7'
+    end
+    piece = board.return_piece(position)
+    return 'No piece found, please try again.' && select_piece(gets.chomp, board) if piece.nil?
+    
+    piece
   end
 
 
