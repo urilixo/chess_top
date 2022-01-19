1fd440e7055daddcf91cd62525c0dc50dd1d88a4
urilixo <falecomelvio@hotmail.com>
2022-01-19 14:52:52 -0300

    Remove position variables

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class Piece
-  attr_accessor :position, :color, :valid_movements
+  attr_accessor :color, :valid_movements
   attr_reader :symbol
 
-  def initialize(color, position)
+  def initialize(color)
     @color = color
-    @position = position
     @valid_movements = []
     @starting_position = true
   end
