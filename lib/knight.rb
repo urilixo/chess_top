8f6fffb4366049e8f0deaec4617e40b32d36c612
urilixo <falecomelvio@hotmail.com>
2022-01-19 18:35:13 -0300

    Remove positional values

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,25 +1,19 @@
 class Knight < Piece
-  def initialize(color, position)
+  def initialize(color)
     super
     @symbol = @color == 'white' ? ' ♘ ' : ' ♞ '
   end
 
-  def return_valid(board)
-    row, col = @position
-    valid_moves = movement(row, col, board)
-    @valid_movements = valid_moves
-  end
-
   def movement(row, col, board)
-    valid = []
+    moves = []
     places = [[-1, -2], [-2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2]]
     places.each do |cell|
       x, y = cell
       next if (row + x).negative? || (col + y).negative? || (row + x) > 7 || col + y > 7
 
       new_position = [row + x, col + y]
-      valid << new_position unless same_color?(new_position, board)
+      moves += [new_position] unless same_color?(new_position, board)
     end
-    valid
+    moves
   end
 end
