f7c58e446b3b92471a01825ac9ee5b85b6c96421
urilixo <falecomelvio@hotmail.com>
2022-01-19 14:10:03 -0300

    Remove position attribute

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,11 +1,14 @@
 class Rook < Piece
-  def initialize(color, position)
+  def initialize(color)
     super
     @symbol = @color == 'white' ? ' ♖ ' : ' ♜ '
   end
 
   def movement(row, col, board)
+    @starting_position = false
+    moves = []
     directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    directions.each { |cell| find_edge(row, col, cell, board)}
+    directions.each { |cell| moves += find_edge(row, col, cell, board) unless find_edge(row, col, cell, board).nil?}
+    moves
   end
 end
