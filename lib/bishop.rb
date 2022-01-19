1e5c02d45ac94fb81558948c708a31c4b72dfa58
urilixo <falecomelvio@hotmail.com>
2022-01-19 14:09:07 -0300

    Remove position attribute

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -1,11 +1,13 @@
 class Bishop < Piece
-  def initialize(color, position)
+  def initialize(color)
     super
     @symbol = @color == 'white' ? ' ♗ ' : ' ♝ '
   end
 
   def movement(row, col, board)
+    moves = []
     directions = [[-1, -1], [-1, 1], [1, 1], [1, -1]]
-    directions.each { |cell| find_edge(row, col, cell, board) }
+    directions.each { |cell| moves += find_edge(row, col, cell, board) unless find_edge(row, col, cell, board).nil? }
+    moves
   end
 end
