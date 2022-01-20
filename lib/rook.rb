ca386e384dc27cf2286e6ecd372e5d89c113cf05
urilixo <falecomelvio@hotmail.com>
2022-01-19 22:27:49 -0300

    Change attribute_reader to read first_move

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,7 +1,10 @@
 class Rook < Piece
+  attr_reader :first_move
+
   def initialize(color)
     super
     @symbol = @color == 'white' ? ' ♖ ' : ' ♜ '
+    @first_move = true
   end
 
   def movement(row, col, board)
@@ -9,6 +12,7 @@ class Rook < Piece
     moves = []
     directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     directions.each { |cell| moves += find_edge(row, col, cell, board) unless find_edge(row, col, cell, board).nil?}
+    @first_move = false
     moves
   end
 end
