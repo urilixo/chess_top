cf6996b08c8b72570514ff267ba3f6a72b8e5ae9
urilixo <falecomelvio@hotmail.com>
2022-01-20 11:43:53 -0300

    Add game_loop method and input validation

--- lib/game.rb
+++ lib/game.rb
@@ -11,6 +11,8 @@ class Game
       @player2 = Player.new('black', player2)
       @board = Board.new
       @current_turn = @player1
+      @checkmate = false
+      game_loop
     end
   end
 
@@ -21,6 +23,7 @@ class Game
       f.write(@player2.to_yaml)
       f.write(@board.to_yaml)
       f.write(@current_turn.to_yaml)
+      f.write(@checkmate.to_yaml)
     end
   end
 
@@ -31,34 +34,43 @@ class Game
       @player2 = array[1]
       @board = array[2]
       @current_turn = array[3]
+      @checkmate = array[4]
     end
   end
 
   def end_turn
-    @current_turn == @player1 ? @player2 : @player1
+    @current_turn = @current_turn == @player1 ? @player2 : @player1
   end
 
-  def make_move
-    player = @player1.color == @current_turn ? @player1 : @player2
-    puts "#{player.name}'s turn, pick a piece to move:"
-    selected_piece, starting_pos = @current_turn.select_piece(gets.chomp, @board)
-    moves = return_valid(selected_piece, starting_pos)
-    puts 'Valid movements are: '
-    moves.each do |move|
-      x, y = move
-      board.selection_color(board.return_piece(move), x, y)
-      board.print_board
+  def game_loop
+    until @checkmate
+      @board.print_board
+      #player = @player1.color == @current_turn ? @player1 : @player2
+      puts "#{@current_turn.name}'s turn, pick a piece to move:"
+      selected_piece, starting_pos = @current_turn.select_piece(board)
+      moves = return_valid(selected_piece, starting_pos)
+      moves.each do |move|
+        x, y = move
+        board.selection_color(board.return_piece(move), x, y)
+        board.print_board
+      end
+      if moves.empty?
+        puts 'No moves for this piece, pick another piece'
+        sleep(2)
+        next
+      end
+      selected_move = valid_move(selected_piece)
+      @board.reset_background
+      @board.move_piece(starting_pos, selected_move)
+      @board.promotion
+      @board.print_board
+      end_turn
     end
-    selected_move = valid_move(selected_piece)
-    @board.reset_background
-    @board.move_piece(starting_pos, selected_move)
-    @board.promotion
   end
 
   def valid_move(piece)
     puts 'Pick a place to move'
-    input = gets.chomp
-    input = input.split(',').map(&:to_i)
+    input = @current_turn.parse_movement(gets.chomp)
     return input if piece.valid_movements.include?(input)
 
     puts 'Invalid move, please try again'
