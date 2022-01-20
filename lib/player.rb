9083bf10c2dc3e21b11edadfd53786142b220fa9
urilixo <falecomelvio@hotmail.com>
2022-01-20 11:45:19 -0300

    Add method to parse position as character plus number

--- lib/player.rb
+++ lib/player.rb
@@ -15,17 +15,30 @@ class Player
     name
   end
 
-  def select_piece(position, board)
+  def parse_movement(position)
+    keys = { 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7 }
+    x, y = position.split('')
+    x = keys[x.to_sym]
+    y = y.to_i - 1
+    [x, y]
+  end
+
+  def select_piece(board)
+    movement = parse_movement(gets.chomp)
+    validate_piece(movement, board)
+  end
+
+  def validate_piece(position, board)
     begin
       position = position.split(',').map(&:to_i)
     rescue NoMethodError
-      puts 'Input a value in the format number,number. E.g. 2,7'
+      puts 'Input a value in a format such as: a1, g7, h3'
     end
     piece = board.return_piece(position)
-    return 'No piece found, please try again.' && select_piece(gets.chomp, board) if piece.nil?
-
+    if piece.nil? || piece.color != color
+      puts 'No piece for this player at this position, please try again.'
+      return select_piece(board)
+    end
     [piece, position]
   end
-
-
 end
